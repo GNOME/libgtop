864867aeeb8088f4e178472005a710bad629347c
Martin Baulig <martin@src.gnome.org>
1998-08-06 23:34:50 +0000

    Initial revision

---
+++ sysdeps/freebsd/init.c
@@ -0,0 +1,43 @@
+/* $Id$ */
+
+/* Copyright (C) 1995, 1996, 1997 Free Software Foundation, Inc.
+   This file is part of the Gnome Top Library.
+   Contributed by Joshua Sled <jsled@xcf.berkeley.edu>, July 1998.
+
+   The Gnome Top Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The Gnome Top Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the GNU C Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
+
+#include <glibtop.h>
+#include <glibtop/open.h>
+
+static glibtop	_glibtop_global_server;
+glibtop		*glibtop_global_server = NULL;
+
+glibtop *
+glibtop_init_r (glibtop **server,
+		const unsigned long features,
+		const unsigned flags)
+{
+	if (*server != NULL)
+		return *server;
+
+	if (glibtop_global_server == NULL) {
+		glibtop_global_server = &_glibtop_global_server;
+		glibtop_open (glibtop_global_server, "glibtop",
+			      features, flags);
+	}
+	
+	return *server = glibtop_global_server;
+}
