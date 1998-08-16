1dd5bf256839184e634eac4b913b0868b8a29358
Martin Baulig <martin@src.gnome.org>
1998-08-16 19:41:32 +0000

    Removed localhost from list of permitted host and added `server_config.h' again - we need to have this file to make a distribution.

---
+++ src/daemon/server_config.h
@@ -0,0 +1,11 @@
+#define SERVER_PORT		42800
+
+#define SERVER_UID		99
+#define SERVER_GID		99
+
+#define HOST_TABLE_ENTRIES	0
+
+const char *permitted_host_names [HOST_TABLE_ENTRIES] = 
+{  };
+
+unsigned long permitted_hosts [HOST_TABLE_ENTRIES];
