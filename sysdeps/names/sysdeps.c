3393adefa7dbbb9b6194ca1989dd04fb1dca2263
Martin Baulig <martin@src.gnome.org>
1998-12-09 14:52:05 +0000

    Added the missing `proc_args' field.

--- sysdeps/names/sysdeps.c
+++ sysdeps/names/sysdeps.c
@@ -26,8 +26,8 @@ const char *glibtop_names_sysdeps [GLIBTOP_MAX_SYSDEPS] =
 	"features", "cpu", "mem", "swap", "uptime", "loadavg",
 	"shm_limits", "msg_limits", "sem_limits", "proclist",
 	"proc_state", "proc_uid", "proc_mem", "proc_time",
-	"proc_signal", "proc_kernel", "proc_segment", "proc_map",
-	"mountlist", "fsusage", "netload", "ppp"
+	"proc_signal", "proc_kernel", "proc_segment", "proc_args",
+	"proc_map", "mountlist", "fsusage", "netload", "ppp"
 };
 
 const unsigned glibtop_types_sysdeps [GLIBTOP_MAX_SYSDEPS] =
@@ -39,7 +39,7 @@ const unsigned glibtop_types_sysdeps [GLIBTOP_MAX_SYSDEPS] =
 	GLIBTOP_TYPE_ULONG, GLIBTOP_TYPE_ULONG, GLIBTOP_TYPE_ULONG,
 	GLIBTOP_TYPE_ULONG, GLIBTOP_TYPE_ULONG, GLIBTOP_TYPE_ULONG,
 	GLIBTOP_TYPE_ULONG, GLIBTOP_TYPE_ULONG, GLIBTOP_TYPE_ULONG,
-	GLIBTOP_TYPE_ULONG
+	GLIBTOP_TYPE_ULONG, GLIBTOP_TYPE_ULONG
 };
 
 const char *glibtop_labels_sysdeps [GLIBTOP_MAX_SYSDEPS] =
@@ -61,6 +61,7 @@ const char *glibtop_labels_sysdeps [GLIBTOP_MAX_SYSDEPS] =
 	N_("Process Signal information"),
 	N_("Process Kernel Data information"),
 	N_("Process Segment information"),
+	N_("Process Arguments"),
 	N_("Process Memory Map"),
 	N_("Mount List"),
 	N_("File System Usage"),
@@ -87,6 +88,7 @@ const char *glibtop_descriptions_sysdeps [GLIBTOP_MAX_SYSDEPS] =
 	N_("Process Signal information"),
 	N_("Process Kernel Data information"),
 	N_("Process Segment information"),
+	N_("Command line arguments of the process"),
 	N_("Process Memory Map"),
 	N_("List of currently mounted filesystems"),
 	N_("File System Usage"),
