ce4f6609652f50a6975cbce7e41e25e93875fc39
Martin Baulig <martin@home-of-linux.org>
1999-02-22 16:19:36 +0000

    Include a copy of the GNU Library General Public License here.
    1999-02-22  Martin Baulig  <martin@home-of-linux.org>
    
    	* COPYING.LIB: Include a copy of the GNU Library General
    	Public License here.
    
    	* copyright.txt: Set the year to 1998-99 and pretty-format it a
    	little bit. Same in all C source and header files.

--- include/glibtop/sysdeps.h
+++ include/glibtop/sysdeps.h
@@ -1,23 +1,25 @@
 /* $Id$ */
 
-/* Copyright (C) 1995, 1996, 1997 Free Software Foundation, Inc.
-   This file is part of the Gnome Top Library.
+/* Copyright (C) 1998-99 Martin Baulig
+   This file is part of LibGTop 1.0.
+
    Contributed by Martin Baulig <martin@home-of-linux.org>, April 1998.
 
-   The Gnome Top Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public License as
-   published by the Free Software Foundation; either version 2 of the
-   License, or (at your option) any later version.
+   LibGTop is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License,
+   or (at your option) any later version.
 
-   The Gnome Top Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
+   LibGTop is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+   License for more details.
 
    You should have received a copy of the GNU Library General Public
-   License along with the GNU C Library; see the file COPYING.LIB.  If not,
-   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.  */
+   License along with LibGTop; see the file COPYING.LIB.  If not, write
+   to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
 
 #ifndef __GLIBTOP_SYSDEPS_H__
 #define __GLIBTOP_SYSDEPS_H__
