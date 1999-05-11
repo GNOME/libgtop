9e16f6e88ccdd0c351cf7c7f6fb53bfbd26947d5
Martin Baulig <martin@home-of-linux.org>
1999-05-11 12:56:50 +0000

    Fixed implementation of the `state' field; added `ruid' and `rgid' fields.
    1999-05-11  Martin Baulig  <martin@home-of-linux.org>
    
    	* procstate.c: Fixed implementation of the `state' field; added
    	`ruid' and `rgid' fields.

--- sysdeps/osf1/procstate.c
+++ sysdeps/osf1/procstate.c
@@ -27,9 +27,14 @@
 
 #include <glibtop_suid.h>
 
+#include <mach.h>
+#include <mach/mach_types.h>
+#include <mach/task_info.h>
+
 static const unsigned long _glibtop_sysdeps_proc_state =
-(1L << GLIBTOP_PROC_STATE_CMD) + (1L << GLIBTOP_PROC_STATE_STATE) +
-(1L << GLIBTOP_PROC_STATE_UID);
+(1L << GLIBTOP_PROC_STATE_CMD) +
+(1L << GLIBTOP_PROC_STATE_UID) + (1L << GLIBTOP_PROC_STATE_GID) +
+(1L << GLIBTOP_PROC_STATE_RUID) + (1L << GLIBTOP_PROC_STATE_RGID);
 
 /* Init function. */
 
@@ -46,7 +51,8 @@ glibtop_get_proc_state_p (glibtop *server, glibtop_proc_state *buf,
 			  pid_t pid)
 {
 	struct tbl_procinfo procinfo;
-	int ret;
+	int minim_state = 99, ret;
+	task_t thistask;
 
 	glibtop_init_p (server, GLIBTOP_SYSDEPS_PROC_STATE, 0);
 	
@@ -65,14 +71,85 @@ glibtop_get_proc_state_p (glibtop *server, glibtop_proc_state *buf,
 	
 	if (ret != 1) return;
 
+	/* Check whether the process actually exists. */
+	if (procinfo.pi_status == PI_EMPTY) return;
+
+	/* Check whether it is not a zombie. */
+	if (procinfo.pi_status == PI_ZOMBIE) {
+		buf->state = GLIBTOP_PROCESS_ZOMBIE;
+		buf->flags = (1L << GLIBTOP_PROC_STATE_STATE);
+		return;
+	}
+
 	strncpy (buf->cmd, procinfo.pi_comm, sizeof (buf->cmd)-1);
 
 	buf->cmd [sizeof (buf->cmd)-1] = 0;
 
-	buf->state = procinfo.pi_status;
-
-	buf->uid = procinfo.pi_uid;
+	buf->uid = procinfo.pi_svuid;
 	buf->gid = procinfo.pi_svgid;
+	buf->ruid = procinfo.pi_ruid;
+	buf->rgid = procinfo.pi_rgid;
 
 	buf->flags = _glibtop_sysdeps_proc_state;
+
+	/* !!! THE FOLLOWING CODE RUNS SUID ROOT - CHANGE WITH CAUTION !!! */
+
+	glibtop_suid_enter (server);
+                
+	/* Get task structure. */
+	ret = task_by_unix_pid (task_self(), procinfo.pi_pid, &thistask);
+        
+	if (ret == KERN_SUCCESS) {
+		thread_array_t			threadarr;
+		unsigned int			threadarr_l;
+		thread_basic_info_t		threadinfo;
+		thread_basic_info_data_t	threadinfodata;
+		int				j;
+
+		/* Get thread array. */
+		(void) task_threads (thistask, &threadarr, &threadarr_l);
+
+		threadinfo = &threadinfodata;
+		for (j = 0; j < threadarr_l; j++) {
+			unsigned int		threadinfo_l = THREAD_BASIC_INFO_COUNT;
+			int			tret;
+
+			tret = thread_info (threadarr [j], THREAD_BASIC_INFO,
+					    (thread_info_t) threadinfo, &threadinfo_l);
+
+			if (tret == KERN_SUCCESS) {
+          			if (minim_state > threadinfo->run_state) 
+					minim_state=threadinfo->run_state;
+			}
+		}
+	}
+
+	glibtop_suid_leave (server);
+
+	/* !!! END OF SUID ROOT PART !!! */
+        
+	if (ret != KERN_SUCCESS) return;
+
+	switch (minim_state) {
+	case TH_STATE_RUNNING:
+		buf->state = GLIBTOP_PROCESS_RUNNING;
+		break;
+	case TH_STATE_UNINTERRUPTIBLE: 
+		buf->state = GLIBTOP_PROCESS_UNINTERRUPTIBLE;
+		break;
+	case TH_STATE_WAITING:
+		buf->state = GLIBTOP_PROCESS_INTERRUPTIBLE;
+		break;
+	case TH_STATE_STOPPED:      
+	case TH_STATE_HALTED:
+		buf->state = GLIBTOP_PROCESS_STOPPED;
+		break;
+	default:
+		if (ret != KERN_SUCCESS)
+			buf->state = GLIBTOP_PROCESS_ZOMBIE;
+		break;
+	}
+
+	if (buf->state)
+		buf->flags |= (1L << GLIBTOP_PROC_STATE_STATE);
 }
