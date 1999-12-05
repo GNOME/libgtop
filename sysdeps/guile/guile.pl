ff9f90c7e65912d0bedeb1491442ffd3e409fd7e
Martin Baulig <martin@home-of-linux.org>
1999-12-05 13:58:03 +0000

    This is now the low-level guile interface to LibGTop. For each of the
    1999-12-05  Martin Baulig  <martin@home-of-linux.org>
    
    	This is now the low-level guile interface to LibGTop. For
    	each of the LibGTop structures we create a new smob here and
    	let the `glibtop-get-<feature>' functions return such a smob.
    
    	* main.c: New file.
    	(glibtop_boot_guile_main): Initialize "glibtop-global-server" here.
    
    	* guile.h: New file.

--- sysdeps/guile/guile.pl
+++ sysdeps/guile/guile.pl
@@ -2,6 +2,8 @@
 
 require 'guile_types.pl';
 
+die "Usage: $0 features.def structures.def" unless $#ARGV == 2;
+
 $[ = 1;			# set array base to 1
 $, = ' ';		# set output field separator
 $\ = "\n";		# set output record separator
@@ -22,37 +24,92 @@ print '/* guile.c */';
 print "/* This is a generated file.  Please modify `guile.pl' */";
 print '';
 
-print '#include <glibtop.h>';
-print '#include <glibtop/xmalloc.h>';
-print '#include <glibtop/sysdeps.h>';
-print '#include <glibtop/union.h>';
-print '';
-print '#include <guile/gh.h>';
-
+print '#include "guile.h"';
+print '#include "structures.h"';
 print '';
 
 $feature_count = 0;
+$smob_count = 0;
 
-while (<>) {
+$smobs{$smob_count++} = 'glibtop';
+
+open FEATURESDEF, $ARGV[1] or
+  die "open ($ARGV[1]): $!";
+
+while (<FEATURESDEF>) {
   chop;				# strip record separator
   
   if (/^[^\#]/) {
-    &make_output($_);
+    &make_output ($_);
   }
 }
 
-$sep = '';
-$sysdeps = 'void|sysdeps|ulong(';
-for ($nr = 0; $nr < $feature_count; $nr++) {
-    $sysdeps = $sysdeps . '' . $sep . '' . $feature_field{$nr};
-    $sep = ',';
+close FEATURESDEF;
+
+open STRUCTDEF, $ARGV[2] or
+  die "open ($ARGV[2]): $!";
+
+while (<STRUCTDEF>) {
+  chop;				# strip record separator
+  
+  if (/^[^\#]/) {
+    &parse_structure_def ($_);
+  }
 }
-$sysdeps = $sysdeps . ')';
-&make_output($sysdeps);
-print 'void';
-print 'glibtop_boot_guile (void)';
+
+close STRUCTDEF;
+
+$init_smobs_code = sprintf
+  (qq[\tscm_glibtop_smob_tags [GLIBTOP_STRUCTURE_GLIBTOP] = scm_make_smob_type\n\t\t("glibtop", sizeof (glibtop));\n]);
+
+for ($nr = 0; $nr < $smob_count; $nr++) {
+  $smob = $smobs{$nr};
+
+  $init_smobs_code .= sprintf
+    (qq[\tscm_glibtop_smob_tags [GLIBTOP_STRUCTURE_%s] = scm_make_smob_type\n\t\t("%s", sizeof (%s));\n],
+     toupper($smob), $smob, $smob);
+}
+
+open OUTPUT, "> $ARGV[3]" or
+  die "open ($ARGV [3]): $!";
+select OUTPUT;
+
+print qq[/* structures.h */];
+print qq[/* This is a generated file.  Please modify \`guile.pl\' */];
+print '';
+print qq[\#ifndef __GLIBTOP_STRUCTURES_H__];
+print qq[\#define __GLIBTOP_STRUCTURES_H__];
+print '';
+print qq[\#include <glibtop.h>];
+print '';
+print qq[BEGIN_LIBGTOP_DECLS];
+print '';
+
+for ($nr = 0; $nr < $smob_count; $nr++) {
+  $smob = $smobs{$nr};
+
+  printf (qq[\#define %-40s\t%d\n], 'GLIBTOP_STRUCTURE_'.&toupper($smob), $nr);
+}
+
+print '';
+print qq[END_LIBGTOP_DECLS];
+print '';
+print qq[\#endif /* __GLIBTOP_STRUCTURES_H__ */];
+
+close OUTPUT;
+
+select STDOUT;
+
+print qq[void\n];
+print qq[glibtop_boot_guile (void)\n];
 print '{';
-print "#include \"guile.x\"";
+print qq[#ifndef SCM_MAGIC_SNARFER\n];
+print qq[#include "guile.x"\n];
+print qq[#endif\n\n];
+print $init_smobs_code;
+
+print '';
+printf qq[\tglibtop_boot_guile_main ();];
 print '}';
 
 sub make_output {
@@ -68,10 +125,11 @@ sub make_output {
   
   $feature_field{$feature_count} = $feature;
   $feature_count = $feature_count + 1;
+
+  $smobs{$smob_count++} = sprintf (qq[glibtop_%s], $feature);
   
   $total_nr_params = 0;
   
-  $temp_list_count = 0;
   $temp_string_count = 0;
   $have_count_var = 0;
   
@@ -82,9 +140,10 @@ sub make_output {
   $pre_call_code = '';
   $post_call_code = '';
   
-  $local_var_decl_code = sprintf (qq[\tglibtop_%s %s;\n\tSCM list;\n],
-				  $feature, $feature);
-  
+  $local_var_decl_code = sprintf (qq[\tglibtop_%s *%s;\n], $feature, $feature);
+  $local_var_decl_code .= sprintf (qq[\tSCM smob_answer;\n]);
+  $local_var_decl_code .= sprintf (qq[\tglibtop *server;\n]);
+
   $call_param = '';
   $param_decl = '';
   $nr_params = (@params = split(/:/, $param_def, 9999));
@@ -120,21 +179,29 @@ sub make_output {
       }
     }
   }
-  if ($param_decl eq '') {
-    $param_decl = 'void';
+  if (!($param_decl eq '')) {
+    $param_decl = ', '.$param_decl;
   }
   
   $nr_params_field{$feature} = $total_nr_params;
   
   $feature_name = $feature;
   $feature_name =~ s/_/-/;
+
+  $field_list_code = '';
+  
+  $init_server_code = sprintf
+    (qq[\tSCM_ASSERT ((SCM_FALSEP (server_smob) ||\n\t\t    (SCM_NIMP (server_smob)\n\t\t     && (SCM_CAR (server_smob) ==\n\t\t\t scm_glibtop_smob_tags [GLIBTOP_STRUCTURE_GLIBTOP]))),\n\t\t    server_smob, SCM_ARG1, "glibtop-get-%s");\n\n], $feature_name);
+
+  $init_server_code .= sprintf
+    (qq[\tserver = SCM_FALSEP (server_smob) ? glibtop_global_server :\n\t\t(glibtop *) SCM_SMOB_DATA (server_smob);\n\n]);
   
   $scm_proc_code = sprintf
     (qq[SCM_PROC (s_%s, "glibtop-get-%s", %d, 0, 0, glibtop_guile_get_%s);],
-     $feature, $feature_name, $nr_params_field{$feature}, $feature);
-  
+     $feature, $feature_name, $nr_params_field{$feature}+1, $feature);
+
   $func_decl_code = sprintf
-    (qq[static SCM\nglibtop_guile_get_%s (%s)], $feature, $param_decl);
+    (qq[static SCM\nglibtop_guile_get_%s (SCM server_smob%s)], $feature, $param_decl);
   
   if ($retval ne 'void') {
     $local_var_decl_code .= sprintf (qq[\t%s retval;\n], $retval);
@@ -145,15 +212,16 @@ sub make_output {
   } else {
     $prefix = '';
   }
-  
+
   $libgtop_call_code = sprintf
-    (qq[\t%sglibtop_get_%s (&%s%s);\n\n], $prefix, $feature,
+    (qq[\tsmob_answer = scm_make_smob (scm_glibtop_smob_tags [GLIBTOP_STRUCTURE_GLIBTOP_%s]);\n], toupper($feature));
+
+  $libgtop_call_code .= sprintf
+    (qq[\t%s = (glibtop_%s *) SCM_SMOB_DATA (smob_answer);\n\n], $feature, $feature);
+  
+  $libgtop_call_code .= sprintf
+    (qq[\t%sglibtop_get_%s_l (server, %s%s);\n\n], $prefix, $feature,
      $feature, $call_param);
-  
-  $temp_list_code = '';
-  
-  $create_list_code = sprintf
-    ("\tlist = gh_list (gh_ulong2scm (%s.flags),\n\t\t\t", $feature);
   
   $nr_elements = (@elements = split(/:/, $element_def, 9999));
   for ($element = 1; $element <= $nr_elements; $element++) {
@@ -169,36 +237,36 @@ sub make_output {
 	$fields[$field] = $field_parts[1];
 	$field_parts[2] =~ s/\]//;
 	
-	if (!$have_count_var) {
-	  $local_var_decl_code .= sprintf (qq[\tlong _LIBGTOP_tmp_i;\n]);
-	  $have_count_var = 1;
-	}
-	
-	$local_var_decl_code .= sprintf
-	  (qq[\tSCM _LIBGTOP_TEMP_list%d;\n], ++$temp_list_count);
-	
-	$temp_list_code .= sprintf
-	  (qq[\t_LIBGTOP_TEMP_list%d = SCM_EOL;\n], $temp_list_count);
-	
-	$temp_list_code .= sprintf
-	  (qq[\tfor (_LIBGTOP_tmp_i = 0; _LIBGTOP_tmp_i < %s; _LIBGTOP_tmp_i++)\n\t\t_LIBGTOP_TEMP_list%d = gh_append2\n\t\t\t(_LIBGTOP_TEMP_list%d,\n\t\t\t gh_list\n\t\t\t (%s (%s.%s [_LIBGTOP_tmp_i]),\n\t\t\t  SCM_UNDEFINED));\n\n], $field_parts[2], $temp_list_count, $temp_list_count, $typeinfo->{$type}->[0], $feature, $fields[$field]);
-	
-	$create_list_code .= sprintf
-	  (qq[_LIBGTOP_TEMP_list%d,\n\t\t\t], $temp_list_count);
+	$field_name = $field_parts[0];
       } else {
-	$create_list_code .= sprintf
-	  (qq[%s (%s.%s),\n\t\t\t], $typeinfo->{$type}->[0], $feature,
-	   $fields[$field]);
+	$field_name = $fields[$field];
       }
+
+      $field_list_code .= sprintf
+	(qq[gh_symbol2scm ("%s"), \\\n\t\t\t\t], $field_name);
     }
   }
   
-  $create_list_code .= "SCM_UNDEFINED);\n";
+  $scm_fields_code = sprintf
+    (qq[SCM_GLOBAL_VCELL_INIT (s_%s_names, "glibtop-fields-%s", \\\n\t\t       gh_list (%sSCM_UNDEFINED));],
+    $feature, $feature_name, $field_list_code);
+
+  $return_smob_code = sprintf
+    (qq[\treturn smob_answer;]);
   
-  $total = sprintf ("%s\n\n%s\n{\n%s\n\n%s\n%s\n%s%s\n\n%s\n\treturn list;\n}\n",
-		    $scm_proc_code, $func_decl_code, $local_var_decl_code,
+  $total = sprintf ("%s\n\n%s\n\n%s\n{\n%s\n\n%s\n%s\n%s%s\n\n%s\n%s\n}\n",
+		    $scm_proc_code, $scm_fields_code, $func_decl_code,
+		    $local_var_decl_code, $init_server_code,
 		    $pre_call_code, $libgtop_call_code, $post_call_code,
-		    $temp_list_code, $create_list_code);
+		    $return_smob_code);
   
   print $total;
+}
+
+sub parse_structure_def {
+  local($line) = @_;
+  @line_fields = split(/\|/, $line, 9999);
+  $name = $line_fields[1];
+
+  $smobs{$smob_count++} = $name;
 }
