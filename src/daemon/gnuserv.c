012ae5faea5b56d5f2439107c95d69bd8186f8c6
Andrew Sobala <aes@src.gnome.org>
2003-05-11 14:36:55 +0000

    Fix buffer overflow vulnerability. Release 1.0.14.

--- src/daemon/gnuserv.c
+++ src/daemon/gnuserv.c
@@ -200,6 +200,11 @@ permitted (u_long host_addr, int fd)
 
 	auth_data_len = atoi (buf);
 
+	if (auth_data_len < 1 || auth_data_len > sizeof(buf)) {
+	    syslog_message(LOG_WARNING, "Invalid data length supplied by client");
+	    return FALSE;
+	}
+
 	if (timed_read (fd, buf, auth_data_len, AUTH_TIMEOUT, 0) != auth_data_len)
 	    return FALSE;
 
