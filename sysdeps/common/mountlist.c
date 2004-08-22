ef95ef85ff11047474d6dfb423a59fd5c82bc32e
Benoît Dejean <bdejean@src.gnome.org>
2004-08-22 19:30:14 +0000

    Merged FreeBSD Marcus' patch.
    	* mountlist.c: (read_filesystem_list): Merged FreeBSD Marcus' patch.

--- sysdeps/common/mountlist.c
+++ sysdeps/common/mountlist.c
@@ -95,7 +95,7 @@ static struct mount_entry *read_filesystem_list (gboolean need_fs_type);
 #endif
 
 
-#if defined (MOUNTED_GETMNTINFO) && !defined (__NetBSD__) && !defined (__OpenBSD__)
+#if defined (MOUNTED_GETMNTINFO) && !defined (__NetBSD__) && !defined (__OpenBSD__) && !defined(__FreeBSD__)
 static const char *
 fstype_to_string (short t)
 {
@@ -300,7 +300,7 @@ read_filesystem_list (gboolean need_fs_type)
 	me = (struct mount_entry *) g_malloc (sizeof (struct mount_entry));
 	me->me_devname = g_strdup (fsp->f_mntfromname);
 	me->me_mountdir = g_strdup (fsp->f_mntonname);
-#if defined(__NetBSD__) || defined(__OpenBSD__)
+#if defined(__NetBSD__) || defined(__OpenBSD__) || defined(__FreeBSD__)
 	me->me_type = g_strdup (fsp->f_fstypename);
 #else
 	me->me_type = g_strdup (fstype_to_string (fsp->f_type));
