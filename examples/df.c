9aae957919df6397ab8db9bbc1dd6cdc7565f589
Benoît Dejean <bdejean@src.gnome.org>
2004-09-24 12:59:29 +0000

    ../configure.in Added new example, just like 'df'.
    	* .cvsignore:
    	* Makefile.am:
    	* ../configure.in
    	* df.c: (print_fsusage), (main): Added new example, just like 'df'.

---
+++ examples/df.c
@@ -0,0 +1,45 @@
+#include <glibtop.h>
+
+#include <glibtop/fsusage.h>
+#include <glibtop/mountlist.h>
+
+#include <stdio.h>
+
+
+static void print_fsusage(const char *mountpoint)
+{
+  glibtop_fsusage buf;
+
+  glibtop_get_fsusage(&buf, mountpoint);
+
+  printf("%-20s %-10llu %-10llu %-10llu %.1f\n",
+	 mountpoint,
+	 buf.blocks * buf.block_size >> 20,
+	 (buf.blocks - buf.bavail) * buf.block_size >> 20,
+	 buf.bavail * buf.block_size >> 20,
+	 (buf.blocks - buf.bavail) * 100.0 / buf.blocks
+	 );
+}
+
+
+int main()
+{
+  glibtop_mountlist buf;
+  glibtop_mountentry *entries;
+  size_t i;
+
+
+  printf("%-20s %-10s %-10s %-10s %-10s\n",
+	 "Filesystem", "Size", "Used", "Avail", "Use%");
+
+  entries = glibtop_get_mountlist(&buf, FALSE);
+
+  for(i = 0; i < buf.number; ++i)
+    {
+      print_fsusage(entries[i].mountdir);
+    }
+
+  g_free(entries);
+
+  return 0;
+}
