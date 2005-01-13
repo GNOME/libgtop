fcb83e637dfc105d5136de419de865615f883ec2
Benoît Dejean <bdejean@src.gnome.org>
2005-01-13 08:56:54 +0000

    Reworked. Should works with IDE, SCSI. DM support is ready, but i don't
    	* fsusage.c: (get_partition), (get_sys_path), (linux_2_6_0): Reworked.
    	Should works with IDE, SCSI. DM support is ready, but i don't know why
    	their /stat is empty ...

--- sysdeps/linux/fsusage.c
+++ sysdeps/linux/fsusage.c
@@ -5,9 +5,10 @@
 
 #include <glib.h>
 
-#include <mntent.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <linux/kdev_t.h>
 
 #include <stdio.h>
 #include <string.h>
@@ -24,68 +25,97 @@ _glibtop_linux_get_fsusage_read_write(glibtop *server,
  * linux/Documentation/iostats.txt
  */
 
+
+
+
+static char *
+get_partition(const char *mountpoint)
+{
+	FILE *partitions;
+	char *name = NULL;
+	char line[1024];
+	struct stat statb;
+
+	if(stat(mountpoint, &statb) == -1)
+		return NULL;
+
+	g_return_val_if_fail((partitions = fopen("/proc/partitions", "r")), NULL);
+
+	while(fgets(line, sizeof line, partitions))
+	{
+		unsigned major, minor;
+		char dev[32];
+
+		if(sscanf(line, "%u %u %*u %31s", &major, &minor, dev) != 3)
+			continue;
+
+		if(MKDEV(major, minor) != statb.st_dev)
+			continue;
+
+		name = g_strdup(dev);
+		break;
+	}
+
+	fclose(partitions);
+	return name;
+}
+
+
+static char *
+get_sys_path(const char *device)
+{
+	if(g_str_has_prefix(device, "hd") || g_str_has_prefix(device, "sd"))
+	{
+		char *prefix;
+		char *path;
+		size_t offset;
+
+		offset = strcspn(device, "0123456789");
+
+		prefix = g_strdup(device);
+		prefix [offset] = '\0';
+
+		path = g_strdup_printf("/sys/block/%s/%s/stat",
+				       prefix, device);
+
+		g_free(prefix);
+		return path;
+
+	}
+	else
+	{
+		return g_strdup_printf("/sys/block/%s/stat", device);
+	}
+}
+
+
+
 static void linux_2_6_0(glibtop *server, glibtop_fsusage *buf, const char *path)
 {
-  FILE *mtab = setmntent("/etc/mtab", "r");
+	char *device;
+	char *filename;
+	int ret;
+	char buffer[BUFSIZ];
+	char *p;
 
-  struct mntent *emnt;
+	device = get_partition(path);
+	if(!device) return;
 
-  while((emnt = getmntent(mtab)) != NULL)
-    {
-      if(strcmp(emnt->mnt_dir, path) == 0)
-      {
-      char filename[64]; /* magic */
+	filename = get_sys_path(device);
+	g_free(device);
 
-      char buffer[1024]; /* magic */
-      char *p;
+	ret = try_file_to_buffer(buffer, filename);
+	g_free(filename);
 
-      char device[32]; /* magic */
-      unsigned partition;
+	if(ret < 0) return;
 
-      const char *devname = emnt->mnt_fsname;
-      size_t offset;
+	p = buffer;
+	p = skip_token(p);
+	buf->read = strtoull(p, &p, 0);
+	p = skip_token(p);
+	buf->write = strtoull(p, &p, 0);
 
-      /*
-	we only deal with /dev block devices "/dev/<DEVICE><PARTITION>"
-	i don't know if other block devices such as network block
-	devices provide this kind of information.
-      */
-
-      if(!g_str_has_prefix(devname, "/dev/"))
-	break;
-
-      /* skip the "/dev/" */
-      devname += sizeof "/dev/" - 1;
-
-      g_strlcpy(device, devname, sizeof device);
-
-      offset = strcspn(devname, "0123456789");
-      partition = strtoul(devname + offset, NULL, 0);
-
-      device[offset] = '\0';
-
-      if((size_t) g_snprintf(filename, sizeof filename,
-			     "/sys/block/%s/%s%u/stat",
-			     device, device, partition) >= sizeof filename)
-	break;
-
-
-      if(try_file_to_buffer(buffer, filename) < 0)
-	break;
-
-      p = buffer;
-      p = skip_token(p);
-      buf->read = strtoull(p, &p, 0);
-      p = skip_token(p);
-      buf->write = strtoull(p, &p, 0);
-
-      buf->flags |= (1 << GLIBTOP_FSUSAGE_READ) | (1 << GLIBTOP_FSUSAGE_WRITE);
-
-      break;
-      }
-    }
-
-  endmntent(mtab);
+	buf->flags |= (1 << GLIBTOP_FSUSAGE_READ) | (1 << GLIBTOP_FSUSAGE_WRITE);
 }
 
 
