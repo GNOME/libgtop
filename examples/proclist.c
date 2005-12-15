101fe5874f6660cfca1d2a19756a746fa70aec77
Benoît Dejean <bdejean@src.gnome.org>
2005-12-15 13:05:55 +0000

    Improved example.
    	* proclist.c: (print_pids), (main):
    
    	Improved example.

--- examples/proclist.c
+++ examples/proclist.c
@@ -5,22 +5,37 @@
 
 #include <stdio.h>
 
-int main()
+#include <unistd.h>
+#include <sys/types.h>
+
+
+static void print_pids(guint64 which, guint64 arg)
 {
 	unsigned *pids;
 	unsigned i;
 	glibtop_proclist buf;
 
-	glibtop_init();
+	pids = glibtop_get_proclist(&buf, which, arg);
 
-	pids = glibtop_get_proclist(&buf, 0, 0);
+	printf("glibtop_get_proclist(%#llx, %llu) -> %lu processes\n",
+	       which, arg, (unsigned long)buf.number);
 
 	for (i = 0; i < buf.number; ++i)
 		printf("%u ", pids[i]);
 
 	putchar('\n');
+	putchar('\n');
 
 	g_free(pids);
+}
+
+
+int main()
+{
+	glibtop_init();
+
+	print_pids(GLIBTOP_KERN_PROC_ALL, 0);
+	print_pids(GLIBTOP_KERN_PROC_UID, getuid());
 
 	glibtop_close();
 
