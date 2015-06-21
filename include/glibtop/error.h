edd08e6fd67085f6f1d273dc544b4755beee5299
Benoit Dejean <bdejean@gmail.com>
2015-06-21 21:04:35 +0200

    Fix macro definition.

--- include/glibtop/error.h
+++ include/glibtop/error.h
@@ -51,7 +51,7 @@ void glibtop_warn_io (const char *format, ...) G_GNUC_PRINTF(1, 2);
 	G_STMT_START {							\
 	if (LIBGTOP_ENABLE_DEBUG)					\
 		glibtop_debug_r(glibtop_global_server, fmt, __VA_ARGS__); \
-	G_STMT_END
+	} G_STMT_END
 
 void glibtop_debug_vr (glibtop *server, const char *format, va_list args);
 void glibtop_debug_r (glibtop *server, const char *format, ...) G_GNUC_PRINTF(2, 3);
