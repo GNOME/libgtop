a9cad9640a37b80793f260beb8e10d6d9c6592d1
Karuna Grewal <karunagrewal98@gmail.com>
2017-12-14 13:06:36 +0530

    Add file parsing for /proc/net/tcp

---
+++ sysdeps/linux/netsockets.c
@@ -0,0 +1,97 @@
+#include "netsockets.h"
+//include the parser header to get the hash map
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ftw.h>
+#include "proc_inode_parser.h"
+gpointer 
+match_pid(int inode, GHashTable *inode_table)
+{
+	if(g_hash_table_contains(inode_table, (gconstpointer *)&inode))
+	{
+		gpointer pid_ptr = g_hash_table_lookup(inode_table,(gconstpointer *)&inode);
+	    printf("%d\n",*(int *)pid_ptr );
+        return pid_ptr;
+    }
+	return NULL;
+}
+
+glibtop_socket *
+add_socket_list(char *buf, glibtop_socket *list_socket,GHashTable *inode_table)
+{	
+	char temp_local_addr[128] ;
+	char temp_rem_addr[128];
+	glibtop_socket *temp_socket = (glibtop_socket *)malloc(sizeof(glibtop_socket));
+	temp_socket->local_addr =(struct in6_addr *)malloc(sizeof(in6_addr));
+    temp_socket->rem_addr =(struct in6_addr *)malloc(sizeof(in6_addr));
+    
+    if(list_socket!=NULL)
+        list_socket->next = temp_socket;
+    else
+        list_socket = temp_socket;	
+	int matches = sscanf(buf, "%*X: %64[0-9A-Fa-f]:%X %64[0-9A-Fa-f]:%X %*X "
+                            "%*X:%*X %*X:%*X %*X %*d %*d %ld %*512s\n",
+                            temp_local_addr,
+                            &(temp_socket->local_port),
+                            temp_rem_addr,
+                            &(temp_socket->rem_port),
+                            &(temp_socket->inode));
+    if(matches !=5)
+    	{
+    		fprintf(stderr, "Invalid buf\n");
+    		exit(0);
+    	}  
+    temp_socket->pid=*(pid_t *)match_pid(temp_socket->inode, inode_table);	
+    if(strlen(temp_local_addr) >8) //it is IPv6
+    	{
+    		sscanf(temp_local_addr, "%08X%08X%08X%08X",
+    					&((temp_socket->local_addr)->s6_addr32[0]),
+    				    &((temp_socket->local_addr)->s6_addr32[1]),
+    					&((temp_socket->local_addr)->s6_addr32[2]),
+    					&((temp_socket->local_addr)->s6_addr32[3]));
+    		sscanf(temp_rem_addr, "%08X%08X%08X%08X",
+    					&((temp_socket->rem_addr)->s6_addr32[0]),
+    					&((temp_socket->rem_addr)->s6_addr32[1]),
+    					&((temp_socket->rem_addr)->s6_addr32[2]),
+    					&((temp_socket->rem_addr)->s6_addr32[3]));
+    		temp_socket->sa_family = AF_INET6;
+
+    	}    
+    else //it is IPv4
+    	{	
+    		    sscanf(temp_local_addr, "%X", (unsigned int *)&(*(temp_socket->local_addr)));
+    			sscanf(temp_rem_addr, "%X", (unsigned int *)&(*(temp_socket->rem_addr)));
+    			temp_socket->sa_family = AF_INET;
+    	}     
+    temp_socket->next = NULL;
+    return temp_socket;
+}
+
+glibtop_socket*
+glibtop_get_netsockets (char *filename,GHashTable *inode_table)
+{   
+	char line[8192] ;
+	traverse_file("/proc",0,"",inode_table);
+	FILE *fd = fopen(filename, "r");
+
+	if(fd == NULL)
+		{
+			fprintf(stderr, "Error opening file : %s",filename);
+			return NULL;
+		}
+    //removing the file header
+    fgets(line, sizeof(line),fd);
+    //to initialise socket_list with the first entry  
+    fgets(line, sizeof(line),fd);
+    glibtop_socket *socket_list=NULL;
+    socket_list = add_socket_list(line,socket_list,inode_table);
+    glibtop_socket *temp_socket_list = socket_list;
+    glibtop_socket *next_socket = temp_socket_list;
+	while(!feof(fd))
+	{	
+        fgets(line, sizeof(line),fd);
+		next_socket = add_socket_list(line,next_socket,inode_table);
+	}
+	return socket_list;
+}
---
+++ sysdeps/linux/netsockets.h
@@ -0,0 +1,31 @@
+#ifndef __NETSOCKETS_H__
+#define __NETSOCKETS_H__
+
+//includes
+#include <glib.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+
+//G_BEEGIN_DECLS
+
+typedef struct _glibtop_socket glibtop_socket;
+
+struct _glibtop_socket
+{
+	char *proc_name;
+	pid_t pid;
+	unsigned long inode;
+	in6_addr *rem_addr;
+	in6_addr *local_addr;
+	int local_port;
+	int rem_port;
+	int sa_family;
+	glibtop_socket *next;
+
+};
+
+glibtop_socket*
+glibtop_get_netsockets (char *filename ,GHashTable *inode_table);
+//G_END_DECLS
+#endif
+
