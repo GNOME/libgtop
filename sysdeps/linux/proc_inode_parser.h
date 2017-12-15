291d555db9a3d7db2b883a0394f31ef963f04c8a
Karuna Grewal <karunagrewal98@gmail.com>
2017-12-15 14:31:55 +0530

    Add hashTable to store inode and pid

--- sysdeps/linux/proc_inode_parser.h
+++ sysdeps/linux/proc_inode_parser.h
@@ -2,11 +2,9 @@
 #define __PROC_INODE_PARSER_H__
 #include <glib.h>              
 G_BEGIN_DECLS
-//hash map global
-
-
 
 GHashTable* traverse_file(const char *dirname, int depth, char *pid, GHashTable *inode_table);
+void print_inode(GHashTable *inode_table);
 
 G_END_DECLS
 
