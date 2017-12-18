4f4127fd547c0200b4bd2f862dd7585024f79039
Karuna Grewal <karunagrewal98@gmail.com>
2017-12-18 16:07:59 +0530

    dev_handles : Open pcap handles for network interfaces

---
+++ sysdeps/linux/dev_handles.c
@@ -0,0 +1,116 @@
+#include <glibtop/netlist.h>
+#include <stdio.h>
+#include <pcap/pcap.h>
+#include "dev_handles.h"
+#include "packet.h"
+#include <tcp.h>
+#include <sys/time.h>
+
+int promisc = 0;
+char errbuf[PCAP_ERRBUF_SIZE];
+timeval curtime;
+
+int process_ip(u_char *userdata, const pcap_pkthdr * /* header */,
+               const u_char *m_packet) {
+	/*TWIP*/
+	return 0;
+}
+int process_ip6(u_char *userdata, const pcap_pkthdr * /* header */,
+               const u_char *m_packet) 
+{
+	/*WIP*/
+	return 0;
+}
+int process_tcp(u_char *userdata, const pcap_pkthdr *header /* header */,
+               const u_char *m_packet) 
+{	/*WIP*/
+	packet_args *args = (packet_args *)userdata;
+	struct tcphdr *tcp = (struct tcphdr *)m_packet;
+
+	curtime = header->ts;
+	Packet *packet = (Packet *)malloc(sizeof(Packet));
+	switch(args->sa_family)
+	{
+	case AF_INET:
+		Packet_init_in_addr(packet, args->ip_src, ntohs(tcp->th_sport), args->ip_dst, ntohs(tcp->th_dport), header->len, header->ts);
+		break;
+
+	case AF_INET6:
+		Packet_init_in6_addr(packet, args->ip6_src, ntohs(tcp->th_sport), args->ip6_dst, ntohs(tcp->th_dport), header->len, header->ts);
+		break;
+	}
+
+}
+
+void add_callback(packet_handle *handle ,enum packet_type type ,packet_callback callback)
+{
+	handle->callback[type] = callback ;
+}
+packet_handle*
+get_interface_handle(char *device )
+{
+	pcap_t *temp = pcap_open_live(device, BUFSIZ, promisc, 100, errbuf);
+	if(temp == NULL)
+	{
+		fprintf(stderr,"failed to open handle for device :%s",device);
+		return NULL;
+	}
+
+	packet_handle *temp_packet_handle = (packet_handle *)malloc(sizeof(packet_handle));
+	if(temp_packet_handle != NULL)
+	{	
+		temp_packet_handle->pcap_handle = temp;
+		temp_packet_handle->device_name = device;
+		temp_packet_handle->next = NULL;
+		temp_packet_handle->linktype = pcap_datalink(temp);
+		for(int i = 0; i < no_of_packet_types; i++)
+		{
+			temp_packet_handle->callback[i] = NULL;
+		}
+		return temp_packet_handle;
+	}
+	return NULL;
+}
+packet_handle * 
+open_pcap_handles()
+{	glibtop_netlist buf;
+	char **devices;
+	devices = glibtop_get_netlist (&buf);
+	int count=0;
+	packet_handle *previous_handle=NULL , *initial_handle = NULL;
+	int init_ele = 1 ; 
+	while(count < buf.number){
+		packet_handle *new_handle = get_interface_handle(devices[count]);
+		if(new_handle != NULL)
+		{	if(init_ele)
+			{
+				initial_handle = new_handle;
+				init_ele = 0;
+			}
+			add_callback(new_handle, packet_ip, process_ip);
+			add_callback(new_handle, packet_ip6, process_ip6);
+			add_callback(new_handle, packet_tcp, process_tcp);
+			if(previous_handle != NULL)
+				previous_handle->next = new_handle;
+			previous_handle = new_handle;
+		}	
+		count++;
+	}
+	return initial_handle;
+
+}
+void 
+print_pcap_handles(packet_handle *handle)
+{
+	glibtop_netlist buf;
+	char **devices;
+	devices = glibtop_get_netlist (&buf);
+	int count=0;
+	packet_handle *temp_handle = handle;
+	while(temp_handle != NULL)
+	{
+		printf("device name : %s linktype: %d \n ",temp_handle->device_name, temp_handle->linktype);
+		temp_handle = temp_handle->next;
+	}	
+}
+
---
+++ sysdeps/linux/dev_handles.h
@@ -0,0 +1,43 @@
+#ifndef __DEV_HANDLES_H__
+#define __DEV_HANDLES_H__
+
+#include <glib.h>
+#include <pcap.h>
+#include <netinet/in.h>
+G_BEGIN_DECLS
+typedef struct _packet_handle packet_handle;
+//for callbacks
+typedef int (*packet_callback)(u_char* , const pcap_pkthdr*, const u_char *);
+enum packet_type
+{
+	packet_ip,
+	packet_ip6,
+	packet_tcp,
+	packet_udp,
+	no_of_packet_types
+};
+
+struct  _packet_handle
+{	char *device_name;
+	pcap_t *pcap_handle;
+	int linktype;
+	packet_callback callback[no_of_packet_types];
+	packet_handle *next;
+};
+
+typedef struct _packet_args packet_args;
+struct _packet_args
+{
+	const char* device;
+	int sa_family;
+	in_addr ip_src;
+	in_addr ip_dst;
+	in6_addr ip_src;
+	in6_addr ip_dst;
+};
+
+packet_handle * open_pcap_handles();
+void print_pcap_handles(packet_handle *handle);
+G_END_DECLS
+
+#endif
