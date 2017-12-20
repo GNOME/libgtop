d6351f1758e5becd6e7d1e9a789c3c5d8bd54c79
Karuna Grewal <karunagrewal98@gmail.com>
2017-12-20 10:19:41 +0530

    Change the dir name approach to GFile

--- sysdeps/linux/proc_inode_parser.h
+++ sysdeps/linux/proc_inode_parser.h
@@ -1,9 +1,10 @@
 #ifndef __PROC_INODE_PARSER_H__
 #define __PROC_INODE_PARSER_H__
-#include <glib.h>              
+#include <glib.h>    
+#include <gio/gio.h>          
 G_BEGIN_DECLS
 
-GHashTable* traverse_file(const char *dirname, int depth, char *pid, GHashTable *inode_table);
+GHashTable* traverse_file(GFile *curdir, int depth, const gchar *pid, GHashTable *inode_table);
 void print_inode(GHashTable *inode_table);
 
 G_END_DECLS
