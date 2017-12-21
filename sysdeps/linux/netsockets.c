707cb7adaafe66f798bd4d70f36791fde4c9f7c6
Karuna Grewal <karunagrewal98@gmail.com>
2017-12-21 18:09:37 +0530

    coding style fixes

--- sysdeps/linux/netsockets.c
+++ sysdeps/linux/netsockets.c
@@ -10,51 +10,50 @@
 gint 
 match_pid(int inode, GHashTable *inode_table)
 {
-	if(g_hash_table_contains(inode_table, GINT_TO_POINTER(inode)))
+	if (g_hash_table_contains(inode_table, GINT_TO_POINTER(inode)))
 	{
-		return GPOINTER_TO_INT(g_hash_table_lookup(inode_table,GINT_TO_POINTER(inode)));
-    }
+		return GPOINTER_TO_INT(g_hash_table_lookup(inode_table, GINT_TO_POINTER(inode)));
+	}
 	return -1;
 }
 
 glibtop_socket *
-add_socket_list(char *buf, glibtop_socket *list_socket,GHashTable *inode_table)
+add_socket_list(char *buf, glibtop_socket *list_socket, GHashTable *inode_table)
 {	
 	char temp_local_addr[128];
 	char temp_rem_addr[128];
 	glibtop_socket *temp_socket = g_slice_new(glibtop_socket);
 	temp_socket->local_addr = g_slice_new(in6_addr);
-    temp_socket->rem_addr = g_slice_new(in6_addr);
-    
-    if(list_socket!=NULL)
-        list_socket->next = temp_socket;
-    else
-        {
-			list_socket = temp_socket;
-        }
+	temp_socket->rem_addr = g_slice_new(in6_addr);
+
+	if (list_socket != NULL)
+		list_socket->next = temp_socket;
+	else
+		list_socket = temp_socket;
+
 	int matches = sscanf(buf, "%*X: %64[0-9A-Fa-f]:%X %64[0-9A-Fa-f]:%X %*X "
-                            "%*X:%*X %*X:%*X %*X %*d %*d %ld %*512s\n",
-                            temp_local_addr,
-                            &(temp_socket->local_port),
-                            temp_rem_addr,
-                            &(temp_socket->rem_port),
-                            &(temp_socket->inode));
-    if(matches !=5)
+							"%*X:%*X %*X:%*X %*X %*d %*d %ld %*512s\n",
+							temp_local_addr,
+							&(temp_socket->local_port),
+							temp_rem_addr,
+							&(temp_socket->rem_port),
+							&(temp_socket->inode));
+	if (matches != 5)
 	{
 		fprintf(stderr, "Invalid buf\n");
 		exit(0);
-	}  
+	}
 
-    temp_socket->pid =match_pid(temp_socket->inode, inode_table);
-    glibtop_proc_state *proc_buf = g_slice_new(glibtop_proc_state);
-    glibtop_get_proc_state(proc_buf,temp_socket->pid);
-    temp_socket->proc_name = proc_buf->cmd;
-    
-    if(strlen(temp_local_addr) >8) //it is IPv6
+	temp_socket->pid = match_pid(temp_socket->inode, inode_table);
+	glibtop_proc_state *proc_buf = g_slice_new(glibtop_proc_state);
+	glibtop_get_proc_state(proc_buf, temp_socket->pid);
+	temp_socket->proc_name = proc_buf->cmd;
+
+	if (strlen(temp_local_addr) > 8) //it is IPv6
 	{
 		sscanf(temp_local_addr, "%08X%08X%08X%08X",
 					&((temp_socket->local_addr)->s6_addr32[0]),
-				    &((temp_socket->local_addr)->s6_addr32[1]),
+					&((temp_socket->local_addr)->s6_addr32[1]),
 					&((temp_socket->local_addr)->s6_addr32[2]),
 					&((temp_socket->local_addr)->s6_addr32[3]));
 		sscanf(temp_rem_addr, "%08X%08X%08X%08X",
@@ -64,40 +63,40 @@ add_socket_list(char *buf, glibtop_socket *list_socket,GHashTable *inode_table)
 					&((temp_socket->rem_addr)->s6_addr32[3]));
 		temp_socket->sa_family = AF_INET6;
 
-	}    
-    else //it is IPv4
+	}
+	else//it is IPv4
 	{	
-		    sscanf(temp_local_addr, "%X", (unsigned int *)&(*(temp_socket->local_addr)));
+			sscanf(temp_local_addr, "%X", (unsigned int *)&(*(temp_socket->local_addr)));
 			sscanf(temp_rem_addr, "%X", (unsigned int *)&(*(temp_socket->rem_addr)));
 			temp_socket->sa_family = AF_INET;
-	}     
-    temp_socket->next = NULL;
-    return temp_socket;
+	}
+	temp_socket->next = NULL;
+	return temp_socket;
 }
 
 glibtop_socket*
-glibtop_get_netsockets (char *filename,GHashTable *inode_table)
-{   
+glibtop_get_netsockets (char *filename, GHashTable *inode_table)
+{
 	char line[8192] ;
-	traverse_file(g_file_new_for_path("/proc"),0,"",inode_table);
+	traverse_file(g_file_new_for_path("/proc"), 0, "", inode_table);
 	FILE *fd = fopen(filename, "r");
 
-	if(fd == NULL)
+	if (fd == NULL)
 	{
-		fprintf(stderr, "Error opening file : %s",filename);
+		fprintf(stderr, "Error opening file : %s", filename);
 		return NULL;
 	}
-    //removing the file header
-    fgets(line, sizeof(line),fd);
-    //to initialise socket_list with the first entry  
-    fgets(line, sizeof(line),fd);
-    glibtop_socket *socket_list=NULL;
-    socket_list = add_socket_list(line,socket_list,inode_table);
-    glibtop_socket *temp_socket_list = socket_list;
-    glibtop_socket *next_socket = temp_socket_list;
-	while(!feof(fd)){	
-        fgets(line, sizeof(line),fd);
-		next_socket = add_socket_list(line,next_socket,inode_table);
+	//removing the file header
+	fgets(line, sizeof(line), fd);
+	//to initialise socket_list with the first entry  
+	fgets(line, sizeof(line), fd);
+	glibtop_socket *socket_list = NULL;
+	socket_list = add_socket_list(line, socket_list, inode_table);
+	glibtop_socket *temp_socket_list = socket_list;
+	glibtop_socket *next_socket = temp_socket_list;
+	while(!feof(fd)){
+	fgets(line, sizeof(line), fd);
+	next_socket = add_socket_list(line, next_socket, inode_table);
 	}
-	return socket_list;
+return socket_list;
 }
--- sysdeps/linux/proc_inode_parser.c
+++ sysdeps/linux/proc_inode_parser.c
@@ -9,7 +9,7 @@
 #include <stdlib.h>
 
 GHashTable*
-traverse_file(GFile *curdir ,int depth , const gchar *pid, GHashTable *inode_table)
+traverse_file(GFile *curdir, int depth, const gchar *pid, GHashTable *inode_table)
 {
 	GFileEnumerator *entry = g_file_enumerate_children (curdir,
 								G_FILE_ATTRIBUTE_STANDARD_NAME ,
@@ -17,48 +17,48 @@ traverse_file(GFile *curdir ,int depth , const gchar *pid, GHashTable *inode_tab
 								NULL,
 								NULL);
 	GFileInfo *info_buf;
-	gboolean enter_fd=false;
-	if(entry != NULL)
+	gboolean enter_fd = false;
+	if (entry != NULL)
 	{
-		while((info_buf=g_file_enumerator_next_file (entry,NULL,NULL))!= NULL && !enter_fd){
+		while ((info_buf = g_file_enumerator_next_file(entry, NULL, NULL)) != NULL && !enter_fd) {
 			
 			const gchar *currentFile = g_file_info_get_name(info_buf);
 			
-			if(g_file_info_get_file_type(info_buf) == G_FILE_TYPE_DIRECTORY)
+			if (g_file_info_get_file_type(info_buf) == G_FILE_TYPE_DIRECTORY)
 			{
-			    switch(depth)
-	            {
-	            case 0: 
-	            	if(currentFile[0]>48 && currentFile[0]<57 )
+				switch(depth)
+				{
+				case 0:
+					if (currentFile[0] > 48 && currentFile[0] < 57 )
 					{
 						pid = currentFile;
-						traverse_file(g_file_get_child(curdir, g_file_info_get_name(info_buf)), depth+1,pid,inode_table);
+						traverse_file(g_file_get_child(curdir, g_file_info_get_name(info_buf)), depth+1, pid, inode_table);
 					}
 					break;
 				
 				case 1:
-					if(g_strcmp0(currentFile,"fd") == 0)
+					if (g_strcmp0(currentFile,"fd") == 0)
 					{
 						enter_fd = true;
-						traverse_file(g_file_get_child(curdir, g_file_info_get_name(info_buf)),depth+1,pid,inode_table);
+						traverse_file(g_file_get_child(curdir, g_file_info_get_name(info_buf)), depth+1, pid, inode_table);
 					}
 					break;
-	            }
+				}
 			}
-			else if(g_file_info_get_is_symlink(info_buf))
+			else if (g_file_info_get_is_symlink(info_buf))
 			{	
-				gchar *symlink_name = g_build_filename(g_file_get_path(curdir),currentFile,NULL);
-				gchar *slnk_buf = g_file_read_link(symlink_name,NULL);
-				if(slnk_buf != NULL)
+				gchar *symlink_name = g_build_filename(g_file_get_path(curdir), currentFile, NULL);
+				gchar *slnk_buf = g_file_read_link(symlink_name, NULL);
+				if (slnk_buf != NULL)
 				{
 					int inode;
-					int match = sscanf(slnk_buf, "socket:[%d]\n",&inode);
-					if(match==1)
+					int match = sscanf(slnk_buf, "socket:[%d]\n", &inode);
+					if (match == 1)
 					{
 						int pid_int = g_ascii_strtoll(pid, NULL, 0);
 						g_hash_table_insert(inode_table, GINT_TO_POINTER(inode), GINT_TO_POINTER(pid_int));
-		         	}
-	         	}
+					}
+				}
 			}
 		}
 	}
@@ -67,13 +67,13 @@ traverse_file(GFile *curdir ,int depth , const gchar *pid, GHashTable *inode_tab
 }
 
 void 
-print_hashtable(gpointer key,gpointer value,gpointer user_data)
+print_hashtable(gpointer key, gpointer value, gpointer user_data)
 {
-	printf("%d :: %d\n",GPOINTER_TO_INT(value),GPOINTER_TO_INT(key));
+	printf("%d :: %d\n", GPOINTER_TO_INT(value), GPOINTER_TO_INT(key));
 }
 
 void 
 print_inode(GHashTable *inode_table)
 {
-	g_hash_table_foreach(inode_table,(GHFunc)print_hashtable,NULL);
+	g_hash_table_foreach(inode_table, (GHFunc)print_hashtable, NULL);
 }
