830a943013a4ca900242bf79aa51852ce989e5df
Karuna Grewal <karunagrewal98@gmail.com>
2017-12-22 16:27:33 +0530

    Add Interface's local address

---
+++ sysdeps/linux/interface_local_addr.h
@@ -0,0 +1,29 @@
+#ifndef __INTERFACE_LOCAL_ADDR_H_
+#define __INTERFACE_LOCAL_ADDR_H_
+
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <glib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+typedef  struct _local_addr local_addr;
+struct _local_addr
+{
+	in_addr_t addr; //ip in network Byte order
+	struct in6_addr addr6;
+	short int sa_family; 
+	char *ip_text; //to store ip in text form
+	char *device_name;
+	local_addr *next;
+};
+
+void 		local_addr_init(local_addr *laddr, in_addr_t addr_value, char *device, local_addr *next_laddr = NULL);
+void 		local_addr6_init(local_addr *laddr, struct in6_addr addr6_value,char *device, local_addr *next_laddr = NULL);
+gboolean 	local_addr_contains(local_addr *laddr, const in_addr_t &n_addr);
+gboolean 	local_addr6_contains(local_addr *laddr, const struct in6_addr &n_addr);
+local_addr	*get_device_local_addr(const char *device);
+
+
+
+#endif
