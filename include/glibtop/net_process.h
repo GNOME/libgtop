59ae3667f88d369c99d305b1b888e63e58329aea
Karuna Grewal <karunagrewal98@gmail.com>
2018-01-31 15:19:26 +0530

    Change Process_list to GSList

--- include/glibtop/net_process.h
+++ include/glibtop/net_process.h
@@ -36,9 +36,9 @@ float to_kb(u_int64_t bytes);
 float to_kbps(u_int64_t bytes);
 void Net_process_get_kbps(Net_process *proc, float *recvd, float *sent, struct timeval currtime);
 uid_t Net_process_get_uid(Net_process *proc);
-void Net_process_list_init(Net_process_list *plist, Net_process *proc, Net_process_list *next_val);
-Net_process *Net_process_list_get_proc(Net_process_list *plist);
-Net_process_list *get_proc_list_instance(Net_process_list *val);
+GSList *Net_process_list_init(GSList *plist, Net_process *proc);
+Net_process *Net_process_list_get_proc(GSList *plist);
+GSList *get_proc_list_instance(GSList *val);
 Net_process *get_unknown_proc_instance(Net_process *val);
 G_END_DECLS
 #endif
