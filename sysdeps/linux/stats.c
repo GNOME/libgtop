8b234c41acc58d4f51f52ac6568a679f6ea1a8d3
Karuna Grewal <karunagrewal98@gmail.com>
2018-02-04 21:09:27 +0530

    Change the network_stats_entry struct Now a GArray is used for storing the various instances of network_stats_entry struct, so that it's convenient to consume the network stats list of all processes in a more elegant way.

--- sysdeps/linux/stats.c
+++ sysdeps/linux/stats.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 void 
-stat_init(stat_entry *st, char *proc_name, char *device_name, double recv_value, double sent_value, pid_t pid, uid_t uid)
+network_stats_init(network_stats_entry *st, char *proc_name, char *device_name, double recv_value, double sent_value, pid_t pid, uid_t uid)
 {
 	st->proc_name = proc_name;
 	st->device_name = device_name;
@@ -15,17 +15,36 @@ stat_init(stat_entry *st, char *proc_name, char *device_name, double recv_value,
 }
 
 void
-print_entry(stat_entry *st)
+network_stats_print_entry(network_stats_entry *st)
 {
 	printf("pid :%d \t sent_value:%f \trecv value:%f \tname of process:%s device_name:%s\n", st->pid, st->sent_value, st->recv_value, st->proc_name, st->device_name);
 }
 
 void
-print_stat(stat_entry *st, int nproc)
-{
-	for(int i = 0; i < nproc; i++)
-	{
-		print_entry(&st[i]);
+network_stats_print_stat(GArray *stats,int nproc)
+{	GArray *list = stats;
+	for(int index = 0; index < nproc; index++)
+	{	
+		network_stats_entry temp = g_array_index (stats, network_stats_entry, index);
+		network_stats_print_entry(&temp);
+		if(stats->len>index	)
+		stats = g_array_remove_index(stats, index);
 	}
-	free(st);
+	network_stats_get_global_instance(stats);
 }
+GArray *
+network_stats_get_global_instance(GArray *val)
+{
+	static GArray *global_stats = NULL;
+	if (val != NULL)
+		{	
+			global_stats = val;
+		}
+	else	if (global_stats == NULL)
+	{	
+		global_stats = g_array_new (TRUE,FALSE,sizeof(network_stats_entry));
+	}			
+	return global_stats;
+}
+
+
