669d40be60f38e43f432a1269c33ff6c3aceea38
Karuna Grewal <karunagrewal98@gmail.com>
2018-02-15 17:06:18 +0530

    Change flags to enable Daemon for Linux

--- include/glibtop/netstats.h
+++ include/glibtop/netstats.h
@@ -3,15 +3,19 @@
 
 #include <glib.h>
 
+#include <glibtop.h>
+#include <glibtop/global.h>
+
+
 G_BEGIN_DECLS
 
 #define GLIBTOP_NETSTATS_NUMBER	0
 #define GLIBTOP_MAX_NETSTATS 	1
 
-#if !GLIBTOP_SUID_NETSTATS
+#if GLIBTOP_SUID_NETSTATS
 GArray *network_stats_get_global_instance_s(glibtop *server, GArray *val);
-void glibtop_init_netstats_s(glibtop *server);
-void glibtop_init_packet_capture_s (glibtop *server)
+void _glibtop_init_netstats_s(glibtop *server);
+void glibtop_init_packet_capture_s ();
 #endif
 
 G_END_DECLS
