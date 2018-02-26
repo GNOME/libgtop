05e01a131440d75f8a379236e0c2bc1b017c8c16
Karuna Grewal <karunagrewal98@gmail.com>
2018-02-27 00:06:15 +0530

    Remove proc_name from various structs
    Currently proc_name is also one of the fields in various structs
    across the linux related codebase pertaining to network stats .
    Since we already have PID, keeping process name is redundant as it
    can be easily obtained using other functions already present in
    libgtop.
    
    Also the following fields have been commented ,given the fact that
    we might need them somewhere in the near future:
    	* uid
    	* device name

--- include/glibtop/stats.h
+++ include/glibtop/stats.h
@@ -8,10 +8,9 @@ G_BEGIN_DECLS
 typedef struct _network_stats_entry network_stats_entry;
 struct _network_stats_entry
 {
-	char *proc_name;
-	char *device_name;
+	//char *device_name;
 	pid_t pid;
-	uid_t uid;
+	//uid_t uid;
 	double sent_value;
 	double recv_value;
 };
@@ -20,13 +19,12 @@ typedef struct _netstats stats;
 struct  _netstats
 {
 	guint pid;
-	gchar *process_name;
 	gdouble bytes_sent;
 	gdouble bytes_recv;
 };
 
 
-void network_stats_init(network_stats_entry *st, char *proc_name, char *device_name, double recv_value, double sent_value, pid_t pid, uid_t uid);
+void network_stats_init(network_stats_entry *st, double recv_value, double sent_value, pid_t pid);
 void network_stats_print_stat(GArray *stat, int nproc);
 GArray *network_stats_get_global_instance(GArray *val);
 gboolean get_capture_status(gboolean val);
