a28097efd83a8200c91e2e535fc4cc409519090f
Karuna Grewal <karunagrewal98@gmail.com>
2018-02-26 13:26:22 +0530

    Change the condition for keeping the capture active Instead of while(1) use while(/*status of the ongoing capture*/)

--- sysdeps/linux/stats.c
+++ sysdeps/linux/stats.c
@@ -200,33 +200,27 @@ glibtop_init_packet_capture ()
 	printf("\nLocal Addresses\n");
 	print_interface_local_address();
 	packet_args *userdata = g_slice_new(packet_args); 
-	while(1)
-	{	
-		//if cature status is true continue the capture else return 
-		if (get_capture_status(FALSE))
-		{
-			for(packet_handle *current_handle = handles; current_handle != NULL; current_handle = current_handle->next)
-			{	
-				userdata->device = current_handle->device_name;
-				userdata->sa_family = AF_UNSPEC;
-				
-				if (current_handle->pcap_handle == NULL)
-					continue;
-				int retval = packet_dispatch(current_handle, 0, (u_char *)userdata, sizeof(packet_args))	;
-				//printf will be later changed to  gerror
-				if (retval < 0)
-					printf("Error dispatching for device %s \n ", current_handle->device_name);
-			}
-			time_t const now = time(NULL);
-			if (last_refresh_time + refresh_delay <= now)
-			{	
-				last_refresh_time = now;
-				//error in opening file free later//g_slice_free(glibtop_socket, socket_list); //free the socket details struct 
-				do_refresh();
-			}
+	//if cature status is true continue the capture else return 
+	while (get_capture_status(FALSE))
+	{
+		for(packet_handle *current_handle = handles; current_handle != NULL; current_handle = current_handle->next)
+		{	
+			userdata->device = current_handle->device_name;
+			userdata->sa_family = AF_UNSPEC;
+			
+			if (current_handle->pcap_handle == NULL)
+				continue;
+			int retval = packet_dispatch(current_handle, 0, (u_char *)userdata, sizeof(packet_args))	;
+			//printf will be later changed to  gerror
+			if (retval < 0)
+				printf("Error dispatching for device %s \n ", current_handle->device_name);
 		}
-		else
-			return ;
+		time_t const now = time(NULL);
+		if (last_refresh_time + refresh_delay <= now)
+		{	
+			last_refresh_time = now;
+			//error in opening file free later//g_slice_free(glibtop_socket, socket_list); //free the socket details struct 
+			do_refresh();
+		}
 	}
-
 }
