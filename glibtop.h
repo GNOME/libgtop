572b195407ce3d8c78270b1fce6aa8e692b62ed8
James Dominic P. Guana <guana.histark@gmail.com>
2021-04-20 08:00:09 +0000

    Make ndisk comment clear

--- glibtop.h
+++ glibtop.h
@@ -80,8 +80,8 @@ struct _glibtop
 	int socket;			/* Accepted connection of a socket */
 	int ncpu;			/* Number of CPUs, zero if single-processor */
 	int real_ncpu;			/* Real number of CPUs. Only ncpu are monitored */
-	int ndisk;			    /* Number of DISKs, zero if single-disk */
-	int real_ndisk;			/* Number of PHYSICAL DISKs. Only ncpu is monitored */
+	int ndisk;			    /* Number of DISKs,  zero if single-disk. This pertains to disk volumes */
+	int real_ndisk;			/* Number of PHYSICAL DISKs. Only ndisk is monitored for now */
 	unsigned long os_version_code;	/* Version code of the operating system */
 	const char *name;		/* Program name for error messages */
 	const char *server_command;	/* Command used to invoke server */
