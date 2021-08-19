69457ccfa9cbfaa87f9e1a5793ddf62156b0ae8d
Dr. James Dominic P. Guana <jdpguana@histark.com>
2021-08-20 02:39:07 +0800

    Add processor model
    Add processor model
    
    Signed off: Dr. James Dominic P. Guana <jdpguana@histark.com>

--- examples/sysinfo.c
+++ examples/sysinfo.c
@@ -1,7 +1,7 @@
 /* Copyright (C) 1998-99 Martin Baulig
    This file is part of LibGTop 1.0.
 
-   Contributed by James Dominic P. Guana <guana.histark@gmail.com>, May 2020.
+   Contributed by Dr. James Dominic P. Guana <jdpguana@histark.com>, August 2021.
 
    LibGTop is free software; you can redistribute it and/or modify it
    under the terms of the GNU General Public License as published by
